439e79e2a2c1649b6d87b56e6cfeb1a3b916b1bf
jairramirezl <jairr30@gmail.com>
2021-02-17 01:32:32 +0000

    .

--- tipos_de_datos.py
+++ tipos_de_datos.py
@@ -5,77 +5,99 @@ data = ('  GARCIA,gonzalez,        rodriguez  ,FERNANDEZ ,LOPEZ,martinez ,  sanc
 """Dividir el string contenido en la variable `data` en donde se encuentre el 
 primer caracter `\n` y guardar el resultado en una variable llamada `var1`.
 """
+var1=data.split('\n',1)
 
 
 """Guardar el primer elemento de la variable `var1` en una variable llamada 
 `apellidos` y el segundo elemento en una llamada `decimales`.
 """
-
+apellidos=var1[0]
+decimales=var1[1]
+#print(apellidos)
+#print(decimales)
 
 """Dividir el string contenido en la variable `apellidos` en donde se encuentre el 
 el caracter `,` y guardar el resultado en una variable llamada `apellidos_list`.
 """
-
+apellidos_list=apellidos.split(',')
+#print(apellidos_list)
 
 """Dividir el string contenido en la variable `decimales` en donde se encuentre el 
 el caracter `;` y guardar el resultado en una variable llamada `decimales_list`.
 """
-
+decimales_list=decimales.split(';')
+#print(decimales)
 
 """Obtener el elemento en la posición 38 de `decimales_list`, procesarlo para 
 convertirlo en un entero y guardarlo en variable `var2`.
 """
-
+#print(decimales_list[38])
+var2=int(float(decimales_list[38]))
+#print(var2)
 
 """Obtener el valor en la posición que indique dicho entero (`var2`) de la lista 
 `apellidos_list`. Guardar el resultado en variable `apellido`.
 """
-
+apellido=apellidos_list[var2]
+#print(apellido)
 
 """obtener la longitud (cantidad de caracteres) de la variable `apellido` y 
 guardar el resultado en `len_apellido`.
 """
-
+len_apellido=len(apellido)
+#print(len_apellido)
 
 """Remover los espacios en blanco que hayan en la variable `apellido` y guardar 
 resultado en `apellido2`.
 """
-
+apellido2=apellido.strip()
+#print(len(apellido2))
 
 """Guardar en `apellido_es_mayuscula` si `apellido2` es un string que contiene 
 solo letras mayúsculas.
 """
 
+apellido_es_mayuscula= apellido2.islower()
+#print(apellido_es_mayuscula)
 
 """Convertir la variable `apellido2` de mayúsculas a minúsculas si está en mayúsculas, 
 o de minúsculas a mayúsculas si está en minúsculas y guardarla en `apellido3`.
 """
-
+apellido3=apellido2.upper()
+#print(apellido3)
 
 """Crear variable `a_in_apellido` que contenga la cantidad de letras a que contiene 
 la variable `apellido2`.
 """
-
+a_in_apellido=len(apellido2)
+#print(a_in_apellido)
 
 """Crear variable `apellido_end_ez` que contenga si la variable `apellido2` 
 termina con el string `"ez"`.
 """
-
+apellido_end_ez=apellido2.endswith('ez')
+#print(apellido_end_ez)
 
 """Crear tupla llamada `apellidos_tuple` que contenga los elementos desde el 49 
 al 23 (en orden invertido), saltandose de a 1 elemento (49, 47, 45 , etc) de la
 lista `apellidos_list`.
 """
+apellidos_tuple=tuple()
+apellidos_tuple=apellidos_list[-23:-100:-2]
+#print(apellidos_tuple)
 
 
 """Crear variable `apellidos_list2` que contenga una copia de `apellidos_tuple` 
 (con las conversiones que sean necesarias) y agregarle el elemento de la 
 posición 78 de la lista `apellidos_list`.
 """
-
+apellidos_list2=apellidos_tuple
+apellidos_list2.append(apellidos_list[78])
+#print(apellidos_list2)
 
 """Eliminar el 4 elemento de la variable `apellidos_list2`.
 """
+apellidos_list2[3]
 
 
 """Eliminar el elemento igual a "  MORALES" de la variable `apellidos_list2`.

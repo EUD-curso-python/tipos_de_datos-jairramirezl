17d01d4af792f537dde3a471f76b7b5e85bc0a51
jairramirezl <jairr30@gmail.com>
2021-02-18 02:15:11 +0000

    .

--- tipos_de_datos.py
+++ tipos_de_datos.py
@@ -94,19 +94,14 @@ apellidos_tuple=tuple(apellidos_list[49:22:-2])
 (con las conversiones que sean necesarias) y agregarle el elemento de la 
 posición 78 de la lista `apellidos_list`.
 """
-#print(apellidos_tuple)
-#print('-----------------')
 apellidos_list2=list(apellidos_tuple)
-#print(apellidos_list2)
 apellidos_list2.append(apellidos_list[78])
-#print(apellidos_list2)
+tuple(apellidos_list2)
 
 """Eliminar el 4 elemento de la variable `apellidos_list2`.
 """
 #print(len(apellidos_list2))
-del apellidos_list2[3]
-print(apellidos_list2)
-print(len(apellidos_list2))
+apellidos_list2.pop(3)
 
 """Eliminar el elemento igual a "  MORALES" de la variable `apellidos_list2`.
 """
@@ -116,7 +111,8 @@ apellidos_list2.remove('  MORALES')
 """Agregar (concatenar) a la variable `apellidos_list2`, la lista obtenida del
 elemento 56 al 70 de la variable `apellidos_list`.
 """
-apellidos_list2.append(apellidos_list[56:71])
+#apellidos_list2.append(apellidos_list[56:71])
+apellidos_list2.extend(apellidos_list[56:71])
 #print(apellidos_list2)
 
 """Invertir el orden actual de la variable `apellidos_list2`.
@@ -140,12 +136,13 @@ apellidos_set1.append(apellidos_list[67])
 """Agregar a variable `apellidos_set1` los elementos del 68 al 70 de la 
 variable `apellidos_list`.
 """
-apellidos_set1.append(apellidos_list[67:70])
+apellidos_set1.append(apellidos_list[68:71])
 
 """Eliminar el elemento " CAMPOS  " de la variable `apellidos_set1`.
 """
 #print(apellidos_set1)
 #print(apellidos_set1[13])
+apellidos_set1.remove(' CAMPOS  ')
 
 """Crear la variable `apellidos_set3` con la intersección entre `apellidos_set1` 
 y `apellidos_set2`.
